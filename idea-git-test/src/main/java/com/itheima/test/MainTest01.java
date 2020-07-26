aeab258d2f2d5986b5b676ffda25990ff66fcf7d
tom_glb <goodMorning_glob@atguigu.com>
2020-07-26 20:34:58 +0800

    idea-git-test工程version 1

---
+++ idea-git-test/src/main/java/com/itheima/test/MainTest01.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+/*** 
+ * @Auther: hs
+ * @Date: 2020/7/26 20:30 
+ *
+ */
+public class MainTest01 {
+    public static void main(String[] args) {
+        System.out.println("测试IDEA集成Git=======================");
+        System.out.println("测试IDEA集成Git=======================");
+        System.out.println("测试IDEA集成Git=======================");
+        System.out.println("测试IDEA集成Git=======================");
+    }
+}
